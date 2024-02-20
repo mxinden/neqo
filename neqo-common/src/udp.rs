b901f99a102f44dd55751693f3c17a0e26863734
Max Inden <mail@max-inden.de>
2024-02-20 13:03:39 +0100

    ecn default

--- neqo-common/src/udp.rs
+++ neqo-common/src/udp.rs
@@ -147,7 +147,7 @@ mod tests {
 
         sleep(Duration::from_millis(100)).await;
 
-        let tos_tx = IpTos::from((IpTosDscp::default(), IpTosEcn::Ce));
+        let tos_tx = IpTos::from((IpTosDscp::Le, IpTosEcn::default()));
         let datagram = Datagram::new(
             sender.local_addr()?,
             receiver.local_addr()?,
